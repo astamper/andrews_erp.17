7fd38c8776c89fbc0d16956ca55d726f265a2be0
Prelang Builder <builder@prelang.com>
2016-04-01 20:00:39 +0000

    Defined validation for attribute 'email' on model 'ShippingAddress'

--- app/models/shipping_address.rb
+++ app/models/shipping_address.rb
@@ -2,4 +2,5 @@ class ShippingAddress < ActiveRecord::Base
   belongs_to :customer
   has_many :orders
   validates_formatting_of :zip, using: :us_zip
+  validates_formatting_of :email, using: :email
 end
