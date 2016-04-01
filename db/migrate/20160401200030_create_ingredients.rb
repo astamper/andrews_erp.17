ef93186d90b874a26e15024b06f0c2656ddf3531
Prelang Builder <builder@prelang.com>
2016-04-01 20:00:30 +0000

    Generated model Ingredient with migration

---
+++ db/migrate/20160401200030_create_ingredients.rb
@@ -0,0 +1,10 @@
+class CreateIngredients < ActiveRecord::Migration
+  def change
+    create_table :ingredients do |t|
+      t.decimal :quantity
+      t.string :unit
+
+      t.timestamps
+    end
+  end
+end
